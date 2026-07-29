d67ee5ed347cd3bc9f79600c2eb00dc14a5216c4
ancipov <2110267+ancipov@users.noreply.github.com>
2026-07-29 11:05:16 +0300

    fixed cordova-ios 8.1.1

--- hooks/ios/iOSCopyPreferences.js
+++ hooks/ios/iOSCopyPreferences.js
@@ -2,33 +2,24 @@ const path = require('path');
 const fs = require('fs');
 const plist = require('plist');
 const { ConfigParser } = require('cordova-common');
-
 module.exports = function (context) {
     let projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
-    let configXML = path.join(projectRoot, 'config.xml');
-    let configParser = new ConfigParser(configXML);
-    
+    let configParser = new ConfigParser(path.join(projectRoot, 'config.xml'));
     let appName = configParser.name();
-    let infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
+    let candidates = [
+        path.join(projectRoot, 'platforms/ios/App/App-Info.plist'),
+        path.join(projectRoot, 'platforms/ios/' + appName + '/' + appName + '-Info.plist'),
+        path.join(projectRoot, 'platforms/ios/' + appName + '/' + appName + '-info.plist'),
+    ];
+    let infoPlistPath = candidates.find(p => fs.existsSync(p));
+    if (!infoPlistPath) { console.warn('firebase-analytics: Info.plist not found'); return; }
     let obj = plist.parse(fs.readFileSync(infoPlistPath, 'utf8'));
-
-
-    // set NSUserTrackingUsageDescription if EnableAppTrackingTransparencyPrompt is true
     let enableAppTracking = configParser.getPlatformPreference("EnableAppTrackingTransparencyPrompt", "ios");
     if(enableAppTracking == "true" || enableAppTracking == ""){
         let userTrackingDescription = configParser.getPlatformPreference("USER_TRACKING_DESCRIPTION_IOS", "ios");
-        if(userTrackingDescription != ""){
-            obj['NSUserTrackingUsageDescription'] = userTrackingDescription;
-        }
-    }
-    else if(enableAppTracking == "false"){
-        delete obj['NSUserTrackingUsageDescription'];
-    }
-
+        if(userTrackingDescription != ""){ obj['NSUserTrackingUsageDescription'] = userTrackingDescription; }
+    } else if(enableAppTracking == "false"){ delete obj['NSUserTrackingUsageDescription']; }
     let collectionEnabled = configParser.getGlobalPreference("ANALYTICS_COLLECTION_ENABLED");
-    if (collectionEnabled.toLowerCase() == 'false') {
-        obj['FIREBASE_ANALYTICS_COLLECTION_ENABLED'] = false;
-    }
-
+    if (collectionEnabled.toLowerCase() == 'false') { obj['FIREBASE_ANALYTICS_COLLECTION_ENABLED'] = false; }
     fs.writeFileSync(infoPlistPath, plist.build(obj));
 };
